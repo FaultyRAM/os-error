f32b1c8c0a64634ec3865481541eecbe3706c0aa
FaultyRAM <explodingpsu@gmail.com>
2017-09-27 21:22:46 +1000

    Define `Result` and `OsError` types

--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,65 @@
 #![deny(unused_qualifications)]
 #![forbid(unused_results)]
 #![forbid(variant_size_differences)]
+
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
+use std::result;
+
+/// A type alias for `Result<T, OsError>`, provided for convenience.
+pub type Result<T> = result::Result<T, OsError>;
+
+#[derive(Clone, Debug)]
+/// An error type providing human-readable descriptions of platform-specific errors.
+pub struct OsError {
+    /// The raw error code.
+    code: i32,
+    /// A human-readable description of the error.
+    desc: String,
+}
+
+impl OsError {
+    /// Creates an `OsError` from the most recent platform-specific error that occurred.
+    pub fn last_os_error() -> Self {
+        unimplemented!()
+    }
+
+    /// Creates an `OsError` from a raw platform-specific error code.
+    pub fn from_raw_os_error(_code: i32) -> Self {
+        unimplemented!()
+    }
+
+    #[inline]
+    /// Returns the raw error code associated with a platform-specific error.
+    pub fn error_code(&self) -> i32 {
+        self.code
+    }
+
+    #[inline]
+    /// Returns a reference to a string slice describing a platform-specific error.
+    pub fn as_str(&self) -> &str {
+        &self.desc
+    }
+}
+
+impl AsRef<str> for OsError {
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl Display for OsError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        self.desc.fmt(f)
+    }
+}
+
+impl Error for OsError {
+    fn description(&self) -> &str {
+        &self.desc
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        None
+    }
+}
