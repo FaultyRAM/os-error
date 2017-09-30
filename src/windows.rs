1d2249d796188d5ba4a29c118072ff9a76f1c5c7
FaultyRAM <explodingpsu@gmail.com>
2017-09-30 18:25:07 +1000

    [Windows] Replace the `windows-desktop` feature with the `with_windows_modules` function
    This provides greater flexibility to users and avoids the burden of having to maintain a separate code path.

--- src/windows.rs
+++ src/windows.rs
@@ -12,96 +12,15 @@ use std::ptr;
 use OsError;
 use std::ffi::OsString;
 use std::os::windows::ffi::OsStringExt;
-use winapi::shared::minwindef::{DWORD, LPCVOID};
+use winapi::shared::minwindef::{DWORD, HMODULE, LPCVOID};
 use winapi::shared::ntdef::WCHAR;
-use winapi::um::winbase::{self, FORMAT_MESSAGE_FROM_SYSTEM, FORMAT_MESSAGE_IGNORE_INSERTS};
-#[cfg(feature = "windows-desktop")]
-use winapi::shared::winerror::FACILITY_NT_BIT;
-#[cfg(feature = "windows-desktop")]
-use winapi::um::libloaderapi;
-#[cfg(feature = "windows-desktop")]
-use winapi::um::winbase::FORMAT_MESSAGE_FROM_HMODULE;
+use winapi::um::winbase::{self, FORMAT_MESSAGE_FROM_HMODULE, FORMAT_MESSAGE_FROM_SYSTEM,
+                          FORMAT_MESSAGE_IGNORE_INSERTS};
 
 #[inline]
 /// Windows-specific `from_raw_os_error` implementation.
 pub fn from_raw_os_error(code: i32) -> OsError {
-    let mut msg: [u16; 2048] = [0; 2048];
-    let msg_len = format_message_ex(code, &mut msg);
-    let desc = if msg_len == 0 {
-        let ec_s = code.to_string();
-        let fm_ec_s = os_error_code::get_last_error().to_string();
-        concat_string!(
-            "OS Error ",
-            ec_s,
-            " (FormatMessageW() returned error ",
-            fm_ec_s,
-            ")"
-        )
-    } else if let Some(m) = OsString::from_wide(&msg).to_str() {
-        m.trim_right().to_owned()
-    } else {
-        let ec_s = code.to_string();
-        concat_string!(
-            "OS Error ",
-            ec_s,
-            " (FormatMessageW() returned invalid wide string)"
-        )
-    };
-    OsError {
-        code: code,
-        desc: desc,
-    }
-}
-
-#[cfg_attr(feature = "clippy", allow(cast_sign_loss))]
-#[cfg(feature = "windows-desktop")]
-#[inline]
-/// `FormatMessageW` wrapper for desktop apps.
-fn format_message_ex(code: i32, msg: &mut [u16]) -> u32 {
-    /// "NTDLL.DLL" in UTF-16.
-    const NTDLL_DLL: &'static [u16] = &[
-        'N' as _,
-        'T' as _,
-        'D' as _,
-        'L' as _,
-        'L' as _,
-        '.' as _,
-        'D' as _,
-        'L' as _,
-        'L' as _,
-        0,
-    ];
-    let error_code;
-    let ntdll_flag;
-    unsafe {
-        let ntdll_module = libloaderapi::GetModuleHandleW(NTDLL_DLL.as_ptr());
-        if ntdll_module.is_null() {
-            error_code = code;
-            ntdll_flag = 0;
-        } else {
-            error_code = code ^ FACILITY_NT_BIT;
-            ntdll_flag = FORMAT_MESSAGE_FROM_HMODULE;
-        }
-        format_message(
-            ntdll_flag | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-            ntdll_module as *mut _,
-            error_code as DWORD,
-            msg,
-        )
-    }
-}
-
-#[cfg_attr(feature = "clippy", allow(cast_sign_loss))]
-#[cfg(not(feature = "windows-desktop"))]
-#[inline]
-/// `FormatMessageW` wrapper for store apps.
-fn format_message_ex(code: i32, msg: &mut [u16]) -> u32 {
-    format_message(
-        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-        ptr::null(),
-        code as DWORD,
-        msg,
-    )
+    OsError::with_windows_modules(code, &[])
 }
 
 #[cfg_attr(feature = "clippy", allow(cast_possible_truncation))]
@@ -118,5 +37,71 @@ fn format_message(flags: DWORD, module: LPCVOID, code: DWORD, msg: &mut [WCHAR])
             msg.len() as DWORD,
             ptr::null_mut(),
         )
+    }
+}
+
+impl OsError {
+    #[cfg_attr(feature = "clippy", allow(cast_sign_loss))]
+    /// Attempts to retrieve an error description from one of the given Windows modules, before
+    /// falling back to checking the system.
+    ///
+    /// On Windows, many error codes do not necessarily come from the system, but from e.g. system
+    /// drivers and various low-level APIs. Users expecting such error codes must therefore specify
+    /// where the relevant error descriptions reside.
+    pub fn with_windows_modules(code: i32, modules: &[HMODULE]) -> Self {
+        let mut msg: [u16; 2048] = [0; 2048];
+        for module in modules {
+            let msg_len = format_message(
+                FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS,
+                *module as *const _,
+                code as DWORD,
+                &mut msg,
+            );
+            if msg_len > 0 {
+                return Self::build_os_error(code, &msg[..msg_len as usize]);
+            }
+        }
+        let msg_len = format_message(
+            FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+            ptr::null(),
+            code as DWORD,
+            &mut msg,
+        );
+        if msg_len > 0 {
+            Self::build_os_error(code, &msg[..msg_len as usize])
+        } else {
+            let ec_s = code.to_string();
+            let fm_ec_s = os_error_code::get_last_error().to_string();
+            let desc = concat_string!(
+                "OS Error ",
+                ec_s,
+                " (FormatMessageW() returned error ",
+                fm_ec_s,
+                ")"
+            );
+            Self {
+                code: code,
+                desc: desc,
+            }
+        }
+    }
+
+    #[inline]
+    /// Converts an error code into an `OsError`.
+    fn build_os_error(code: i32, msg: &[u16]) -> Self {
+        let desc = if let Some(m) = OsString::from_wide(msg).to_str() {
+            m.trim_right().to_owned()
+        } else {
+            let ec_s = code.to_string();
+            concat_string!(
+                "OS Error ",
+                ec_s,
+                " (FormatMessageW() returned invalid wide string)"
+            )
+        };
+        Self {
+            code: code,
+            desc: desc,
+        }
     }
 }
